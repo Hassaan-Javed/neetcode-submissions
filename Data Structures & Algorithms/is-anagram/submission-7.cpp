edee23a733388f6d3e681b7d570826baaf598933
Hassaan Javed <102184657+Hassaan-Javed@users.noreply.github.com>
2026-04-28 10:13:15 +0500

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    bool isAnagram(string s, string t) // hiiii hello
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+
+        unordered_map<char, int> sMap;
+        unordered_map<char, int> tMap;
+
+        for (size_t i = 0; i < s.length(); i++)
+        {
+
+            sMap[s[i]]++;
+
+            tMap[t[i]]++;
+        }
+
+        for (size_t i = 0; i < s.length(); i++)
+        {
+            if (tMap[s[i]] != sMap[s[i]])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
