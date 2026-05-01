4d828cbc8af87d14cba32eaf11199cb235fbe6bc
Hassaan Javed <102184657+Hassaan-Javed@users.noreply.github.com>
2026-05-01 20:16:00 +0500

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {//{0}
+
+        int product=1;
+
+        int zeroCount=0;
+        vector<int> productArray;
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if (nums[i]!=0)
+            {
+                product*=nums[i];
+            }else{
+                zeroCount++;
+            }
+            
+        }
+
+
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+
+            if (zeroCount!=0 && nums[i]!=0)
+            {
+                productArray.push_back(0);
+                
+            }else{
+             
+
+                if (nums[i]!=0)
+                {
+                    productArray.push_back(product/nums[i]);
+                }else{
+
+                    if (zeroCount>1)
+                    {
+                        productArray.push_back(0);
+
+                    }else{
+                        productArray.push_back(product);
+
+                    }
+                }
+            }
+        }
+        return productArray;
+        
+
+    }
+};
