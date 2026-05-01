272705ab930fdd6a6e48fc33ad6b124baec7ebe3
Hassaan Javed <102184657+Hassaan-Javed@users.noreply.github.com>
2026-05-01 20:12:33 +0500

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {//{0}
+
+        int product=1;
+        set<int> zeroIndices; //0,1
+        vector<int> productArray;
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if (nums[i]!=0)
+            {
+                product*=nums[i];
+            }else{
+                zeroIndices.insert(i);
+            }
+            
+        }
+
+
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+
+            if (!zeroIndices.empty() && zeroIndices.count(i)==0)
+            {
+                productArray.push_back(0);
+                
+            }else{
+             
+
+                if (nums[i]!=0)
+                {
+                    productArray.push_back(product/nums[i]);
+                }else{
+
+                    if (zeroIndices.size()>1)
+                    {
+                        productArray.push_back(0);
+
+                    }else{
+                        productArray.push_back(product);
+
+                    }
+                    
+
+                }
+          
+            }
+        }
+        
+
+        return productArray;
+        
+
+    }
+};
